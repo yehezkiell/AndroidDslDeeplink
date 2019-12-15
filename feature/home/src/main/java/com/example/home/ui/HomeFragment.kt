956f65a10fc91c78cc8bd3647320aee99521ecb0
yehezkiel <yehezkiel@tokopedia.com>
2019-12-15 18:47:02 +0700

    add mediator livedata simple example

--- feature/home/src/main/java/com/example/home/ui/HomeFragment.kt
+++ feature/home/src/main/java/com/example/home/ui/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import com.example.abstraction.base.BaseDaggerFragment
 import com.example.abstraction.di.MainApplication
@@ -32,6 +33,14 @@ class HomeFragment : BaseDaggerFragment() {
         super.onViewCreated(view, savedInstanceState)
         Toast.makeText(context!!, viewModel.getInfo(), Toast.LENGTH_LONG).show()
 
+        sumByOne.setOnClickListener {
+            viewModel.addNumberOne()
+        }
+
+        sumByTwo.setOnClickListener {
+            viewModel.addNumberTwo()
+        }
+
         txt_home.setOnClickListener {
             val uri: Uri = Uri.parse("http://www.nbageek.com/team").buildUpon()
                 .appendQueryParameter("id","123")
@@ -42,5 +51,12 @@ class HomeFragment : BaseDaggerFragment() {
         txt_match_detail.setOnClickListener {
             view.findNavController().navigate(R.id.actionHome_To_Matchdetail)
         }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.sumResult.observe(this, Observer {
+            sumResult.text = "Sum Result: $it"
+        })
     }
 }
--- feature/home/src/main/java/com/example/home/ui/HomeViewModel.kt
+++ feature/home/src/main/java/com/example/home/ui/HomeViewModel.kt
@@ -1,8 +1,34 @@
 package com.example.home.ui
 
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
+    private val numberOne = MutableLiveData<Int>()
+    private val numberTwo = MutableLiveData<Int>()
+
+    val sumResult = MediatorLiveData<Int>().apply {
+        postValue(0)
+    }
+
+    init {
+        sumResult.addSource(numberOne) {
+            sumResult.value = sumResult.value?.plus(it)
+        }
+
+        sumResult.addSource(numberTwo) {
+            sumResult.value = sumResult.value?.plus(it)
+        }
+    }
+
+    fun addNumberOne() {
+        numberOne.value = 1
+    }
+
+    fun addNumberTwo() {
+        numberOne.value = 2
+    }
 
     fun getInfo(): String {
         return "Hai"
